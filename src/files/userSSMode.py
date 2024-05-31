3b480d76d4b8e9be9cad33c7d1131064104274e5
Kcisti <91692567+Kcisti@users.noreply.github.com>
2024-05-31 13:42:39 +0200

    v1.1

--- src/files/userSSMode.py
+++ src/files/userSSMode.py
@@ -6,42 +6,56 @@ def userSSModeF():
         username = input(f"\n {Wh}Enter Username : {Gr}")
         results = {}
         social_media = [
-            {"url": "https://www.facebook.com/{}", "name": "Facebook"},
-            {"url": "https://www.twitter.com/{}", "name": "Twitter"},
-            {"url": "https://www.instagram.com/{}", "name": "Instagram"},
-            {"url": "https://www.linkedin.com/in/{}", "name": "LinkedIn"},
-            {"url": "https://www.github.com/{}", "name": "GitHub"},
-            {"url": "https://www.pinterest.com/{}", "name": "Pinterest"},
-            {"url": "https://www.tumblr.com/{}", "name": "Tumblr"},
-            {"url": "https://www.youtube.com/{}", "name": "Youtube"},
-            {"url": "https://soundcloud.com/{}", "name": "SoundCloud"},
-            {"url": "https://www.snapchat.com/add/{}", "name": "Snapchat"},
-            {"url": "https://www.tiktok.com/@{}", "name": "TikTok"},
-            {"url": "https://www.behance.net/{}", "name": "Behance"},
-            {"url": "https://www.medium.com/@{}", "name": "Medium"},
-            {"url": "https://www.quora.com/profile/{}", "name": "Quora"},
-            {"url": "https://www.flickr.com/people/{}", "name": "Flickr"},
-            {"url": "https://www.periscope.tv/{}", "name": "Periscope"},
-            {"url": "https://www.twitch.tv/{}", "name": "Twitch"},
-            {"url": "https://www.dribbble.com/{}", "name": "Dribbble"},
-            {"url": "https://www.stumbleupon.com/stumbler/{}", "name": "StumbleUpon"},
-            {"url": "https://www.ello.co/{}", "name": "Ello"},
-            {"url": "https://www.producthunt.com/@{}", "name": "Product Hunt"},
-            {"url": "https://www.snapchat.com/add/{}", "name": "Snapchat"},
-            {"url": "https://www.telegram.me/{}", "name": "Telegram"},
-            {"url": "https://www.weheartit.com/{}", "name": "We Heart It"}
+            {"url": "https://www.facebook.com/{}", "name": "Facebook", "category" : "social"},
+            {"url": "https://www.twitter.com/{}", "name": "Twitter", "category" : "social"},
+            {"url": "https://www.instagram.com/{}", "name": "Instagram", "category" : "social"},
+            {"url": "https://www.linkedin.com/in/{}", "name": "LinkedIn", "category" : "other"},
+            {"url": "https://www.github.com/{}", "name": "GitHub", "category" : "hobby"},
+            {"url": "https://www.pinterest.com/{}", "name": "Pinterest", "category" : "social"},
+            {"url": "https://www.tumblr.com/{}", "name": "Tumblr", "category" : "social"},
+            {"url": "https://www.youtube.com/{}", "name": "Youtube", "category" : "other"},
+            {"url": "https://soundcloud.com/{}", "name": "SoundCloud", "category" : "hobby"},
+            {"url": "https://www.snapchat.com/add/{}", "name": "Snapchat", "category" : "social"},
+            {"url": "https://www.tiktok.com/@{}", "name": "TikTok", "category" : "social"},
+            {"url": "https://www.behance.net/{}", "name": "Behance", "category" : "other"},
+            {"url": "https://www.medium.com/@{}", "name": "Medium", "category" : "forum"},
+            {"url": "https://www.quora.com/profile/{}", "name": "Quora", "category" : "forum"},
+            {"url": "https://www.flickr.com/people/{}", "name": "Flickr", "category" : "other"},
+            {"url": "https://www.periscope.tv/{}", "name": "Periscope", "category" : "other"},
+            {"url": "https://www.twitch.tv/{}", "name": "Twitch", "category" : "hobby"},
+            {"url": "https://www.dribbble.com/{}", "name": "Dribbble", "category" : "other"},
+            {"url": "https://www.stumbleupon.com/stumbler/{}", "name": "StumbleUpon", "category" : "other"},
+            {"url": "https://www.ello.co/{}", "name": "Ello", "category" : "other"},
+            {"url": "https://www.producthunt.com/@{}", "name": "Product Hunt", "category" : "other"},
+            {"url": "https://www.telegram.me/{}", "name": "Telegram", "category" : "social"},
+            {"url": "https://www.weheartit.com/{}", "name": "We Heart It", "category" : "other"},
+            {"url": "https://bere.al/{}", "name":"Bereal", "category" : "social"},
+            {"url": "https://www.chess.com/topic/{}?search=1", "name":"Chess", "category" : "hobby"},
+            {"url": "https://open.spotify.com/user/{}", "name":"Spotify", "category" : "hobby"},
+            {"url": "https://www.wattpad.com/{}", "name":"Wattpad", "category" : "hobby"},
+            {"url": "https://lichess.org/@/{}", "name":"Lichess", "category" : "hobby"},
         ]
+        social,hobby,forum,other=0,0,0,0
         for site in social_media:
             url = site['url'].format(username)
             response = requests.get(url)
             if response.status_code == 200:
                 results[site['name']] = url
+                if site['category'] == 'social':
+                    social=social+1
+                elif site['category'] == 'hobby':
+                    hobby=hobby+1
+                elif site['category'] == 'forum':
+                    forum=forum+1
+                elif site['category'] == 'other':
+                    other=other+1
             else:
                 results[site['name']] = (f"{Re}! Username not found !")
     except Exception as e:
         print(f"{Re}Error : {e}")
         return
-    print(f"\n{Gr} SHOW INFORMATION USERNAME")
-    print()
+
+    print(f"\n{Gr} SHOW INFORMATION USERNAME\n")
     for site, url in results.items():
         print(f"{Wh} {Gr}+ {Wh} {site} : {Gr}{url}")
+    print(f"\n FOUND: {hobby} hobbies, {social} social, {forum} forums and {other} others")
